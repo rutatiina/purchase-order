1e0a380a595bed5c71b10cc9f874341328c7a2e1
rutatiina <rutatiina@ruginem.org>
2022-10-10 11:42:25 +0300

    Add soft deleting to all models

--- src/Models/PurchaseOrder.php
+++ src/Models/PurchaseOrder.php
@@ -3,11 +3,13 @@
 namespace Rutatiina\PurchaseOrder\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Activitylog\Traits\LogsActivity;
 use Rutatiina\Tenant\Scopes\TenantIdScope;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PurchaseOrder extends Model
 {
+    use SoftDeletes;
     use LogsActivity;
 
     protected static $logName = 'Txn';
@@ -50,12 +52,21 @@ class PurchaseOrder extends Model
 
         static::addGlobalScope(new TenantIdScope);
 
-        self::deleting(function($txn) { // before delete() method call this
+        self::deleted(function($txn) { // before delete() method call this
              $txn->items()->each(function($row) {
                 $row->delete();
              });
              $txn->comments()->each(function($row) {
                 $row->delete();
+             });
+        });
+
+        self::restored(function($txn) {
+             $txn->items()->each(function($row) {
+                $row->restore();
+             });
+             $txn->comments()->each(function($row) {
+                $row->restore();
              });
         });
 
